e71ee7d9b02119c29155fa1dccee540118aa7dbb
Adriel Fernando <adrielfernando450@gmail.com>
2024-10-01 16:15:10 -0300

    test git

--- frontend/app-clash/App.js
+++ frontend/app-clash/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>projeto clash!</Text>
+      <Text>projeto clash!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
