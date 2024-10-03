93b8fb16f6f03e83869ef9fa2003f2fe60e2e41f
Adriel Fernando <adrielfernando450@gmail.com>
2024-10-02 21:46:23 -0300

    adição do grid , componentes do react native paper e estruturação dos inputs

--- frontend/app-clash/App.js
+++ frontend/app-clash/App.js
@@ -1,20 +1,169 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { TextInput, Card, Button } from 'react-native-paper'; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [number, setNumber] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>projeto clash!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.app}>
+        
+        <View style={styles.gridContainer}>
+                   
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined" 
+                style={styles.input}
+              />a
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+
+
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text>projeto clash!!!</Text>
+              <TextInput
+                label="Digite um número"
+                value={number}
+                onChangeText={setNumber}
+                keyboardType="numeric"
+                mode="outlined"
+                style={styles.input}
+              />
+            </Card.Content>
+            <Button mode="contained" onPress={() => console.log('Número digitado:', number)} style={styles.button}>
+            Enviar
+          </Button>
+          </Card>          
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  app: {
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    padding: '5%'
   },
+  gridContainer: { 
+    flexDirection: 'row',          
+    flexWrap: 'wrap',              
+    justifyContent: 'center', 
+    width: '100%',                
+    paddingHorizontal: 10,      
+       
+  },
+  card: {
+    width: '45%',
+    marginVertical: '2%',
+    marginHorizontal: '2%',
+    padding: 10,
+  },
+  input: {
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 20,
+    width: '60%',
+    alignSelf: 'center'
+  }
 });
